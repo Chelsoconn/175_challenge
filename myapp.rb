268cc3691e32b1e80f57e41c846b8a9cdb71b2d9
Chelsoconn <c.oconnor1517@gmail.com>
2022-10-25 11:26:41 -0500

    all

---
+++ myapp.rb
@@ -0,0 +1,27 @@
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+  @contents = Dir.glob("*")
+  erb :home 
+end
+
+get "/one.txt" do 
+  @contents= Dir.glob("*")
+  erb :chap1
+end 
+
+get "/two.txt" do 
+  @contents= Dir.glob("*")
+  erb :chap2 
+end 
+
+get "/three.txt" do 
+  @contents= Dir.glob("*")
+  erb :chap3 
+end 
+
+get "/four.txt" do
+  @contents= Dir.glob("*") 
+  erb :chap4 
+end 
